b129dfdfa3c83a5a121fb17135b320b00a2b4c69
Benjamin <bjudkewitz@users.noreply.github.com>
2025-05-04 13:32:11 +0200

    polarity

--- src/opm_unshear/__init__.py
+++ src/opm_unshear/__init__.py
@@ -3,6 +3,7 @@
 """
 
 import logging
+import numpy as np
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
@@ -17,16 +18,17 @@ except (ImportError, cp.cuda.runtime.CUDARuntimeError):
     from .cpu import unshear
     logging.info("GPU not available. Using CPU implementation.")
 
-def get_slope(n1, n2, M1_2, M2_3, dv, dp, theta_iip=None, theta_sample=None):
+def get_slope(n1, n2, M12, M23, dv, dp, polarity=1, theta_iip=None, theta_sample=None):
     """Calculate the slope of the shear.
 
     Args:
         n1 (float): refractive index at the sample (Obj1)
         n2 (float): refractive index at the intermediate image plane (Obj2)
-        M1_2 (float): magnification from Obj1 to Obj2
-        M2_3 (float): magnification from Obj2 to Obj3
+        M12 (float): magnification from Obj1 to Obj2
+        M23 (float): magnification from Obj2 to Obj3
         dv (float): vertical pixel size (on the camera)
         dp (float): plane separation along the plane-scanning axis.
+        polarity (int): scanning polarity (depends on scanning and plane tilt directiona). 1 for positive, -1 for negative.
         theta_iip (float): angle of the intermedia image plane, in radians. Either this or theta_sample must be provided.
         theta_sample (float): angle of the sample plane, in radians. Either this or theta_iip must be provided.
 
@@ -35,15 +37,17 @@ def get_slope(n1, n2, M1_2, M2_3, dv, dp, theta_iip=None, theta_sample=None):
         theta_sample (float): angle of the sample plane, in radians
         theta_iip (float): angle of the intermedia image plane, in radians
     """
-
+    if polarity not in [1, -1]:
+        raise ValueError("Polarity must be 1 or -1.")
     if theta_iip is None and theta_sample is None:
         raise ValueError("Either theta_iip or theta_sample must be provided.")
     if theta_iip is not None and theta_sample is not None:
         raise ValueError("Only one of theta_iip or theta_sample must be provided.")
     if theta_iip is not None:
-        theta_sample = np.arctan(np.tan(theta_iip) * (M1_2 / n1 * n2))
+        theta_sample = np.arctan(np.tan(theta_iip) * (M12 / n1 * n2))
     if theta_sample is not None:
-        theta_iip = np.arctan(np.tan(theta_sample) / (M1_2 / n1 * n2))
-    dz_sample = dv / M2_3 * np.sin(theta_iip) / (M1_2**2) * n1 / n2
+        theta_iip = np.arctan(np.tan(theta_sample) / (M12 / n1 * n2))
+    dz_sample = dv / M23 * np.sin(theta_iip) / (M12**2) * n1 / n2
     slope = (dp / np.tan(theta_sample)) / dz_sample
+    slope = slope * polarity
     return slope, theta_sample, theta_iip
